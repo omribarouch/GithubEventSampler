4da35ce28dfa6932308ca038cadebb4773d24b24
Omri Barouch <omby8888@gmail.com>
2023-12-16 12:14:02 +0200

    docker compose run kafka

--- eventSampler/event_sampler.go
+++ eventSampler/event_sampler.go
@@ -2,64 +2,26 @@ package main
 
 import (
 	"context"
-	"log"
-	"time"
-
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/google/go-github/v38/github"
-	"golang.org/x/oauth2"
+	"fmt"
+	"github.com/segmentio/kafka-go"
 )
 
 func main() {
-	// Set up a context for graceful shutdown
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	// GitHub API token, Kafka broker, and topic
-	token := "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"
-	broker := "localhost:9093"
-	topic := "github-events"
-
-	// Kafka producer configuration
-	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": broker})
-	if err != nil {
-		log.Fatalf("Failed to create Kafka producer: %s", err)
+	conf := kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "github-events",
+		GroupID: "g1",
 	}
-	defer producer.Close()
 
-	// GitHub API client setup
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-
-	// Periodically fetch and send GitHub events to Kafka
-	ticker := time.NewTicker(2 * time.Minute) // Adjust the interval as needed
-
+	reader := kafka.NewReader(conf)
+	fmt.Println("Started listening...")
 	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Event Sampler shutting down.")
-			return
-		case <-ticker.C:
-			events, _, err := client.Activity.ListEvents(ctx, nil)
-			if err != nil {
-				log.Printf("Error fetching GitHub events: %s", err)
-				continue
-			}
-
-			for _, event := range events {
-				// Produce the message to the specified topic
-				err := producer.Produce(&kafka.Message{
-					TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-					Value:          event.GetRawPayload(),
-				}, nil)
-
-				if err != nil {
-					log.Printf("Failed to produce message: %v", err)
-				}
-			}
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			fmt.Println("Error occurred", err)
+			continue
 		}
+
+		fmt.Println("Message is:", string(msg.Value))
 	}
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module GithubEventSampler
 
 go 1.21
 
+require github.com/segmentio/kafka-go v0.4.47
+
 require (
-	github.com/confluentinc/confluent-kafka-go v1.9.2
-	github.com/google/go-github/v38 v38.1.0
-	github.com/gorilla/mux v1.8.1
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 )
